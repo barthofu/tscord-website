dce4b81729fd0af9bb1bdbf735cb377bef5a6eea
Bartholomé <bartho.gili@gmail.com>
2022-08-15 11:56:30 +0200

    feat(next.config): default locale to `en`

--- next.config.js
+++ next.config.js
@@ -2,12 +2,18 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
 	reactStrictMode: true,
 
 	sassOptions: {
         includePaths: [
             path.join(__dirname, 'public/styles/')
         ]
+    },
+
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
     }
 }
 
